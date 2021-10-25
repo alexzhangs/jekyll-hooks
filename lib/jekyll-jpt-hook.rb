9ae81fde2b0e24d44d65ebcfe25a8e92d03563dc
Alex <alexzhangs@gmail.com>
2021-10-26 05:13:08 +0800

    Update jekyll-jpt-hook.rb

--- lib/jekyll-jpt-hook.rb
+++ lib/jekyll-jpt-hook.rb
@@ -1,8 +1,37 @@
-Jekyll::Hooks.register :documents, :pre_render do |document, payload|
-    docExt = document.extname.tr('.', '')
-    # only process if we deal with a markdown file
-    if payload['site']['markdown_ext'].include? docExt
-        newContent = document.content.gsub(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% picture \2 --alt \1 %}')
-        document.content = newContent
+Jekyll::Hooks.register :site, :pre_render do |site, payload|
+  config = site.config['hooks'] || {}
+
+  unless config['disabled']
+    for action in config['actions'] do
+      aType = action['type']
+      aExts = action['exts']
+      aFind = action['find']
+      aReplace = action['replace'] || ''
+      aDisabled = action['disabled']
+
+      if aDisabled
+        next
+      end
+
+      if ['posts', 'pages', 'documents'].include?(aType)
+        for item in site.send(aType) do
+          if aExts
+            docExt = item.extname.tr('.', '')
+            unless aExts.include?(docExt)
+              next
+            end
+          end
+
+          unless aFind
+            next
+          end
+
+          aFind = Regexp.new aFind
+          newContent = item.content.gsub(aFind, aReplace)
+          item.content = newContent
+        end
+      end
+
     end
+  end
 end
