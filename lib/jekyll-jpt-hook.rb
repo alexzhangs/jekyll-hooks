adecd90536c9e54c147d132f32e5bb77eacdd09e
Alex <alexzhangs@gmail.com>
2021-10-25 23:14:09 +0800

    Update jekyll-jpt-hook.rb

--- lib/jekyll-jpt-hook.rb
+++ lib/jekyll-jpt-hook.rb
@@ -1,8 +1,8 @@
-Jekyll::Hooks.register :pages, :pre_render do |page, payload|
-    docExt = page.extname.tr('.', '')
+Jekyll::Hooks.register :documents, :pre_render do |document, payload|
+    docExt = document.extname.tr('.', '')
     # only process if we deal with a markdown file
     if payload['site']['markdown_ext'].include? docExt
-        newContent = page.content.gsub(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% picture \2 --alt \1 %}')
-        page.content = newContent
+        newContent = document.content.gsub(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% picture \2 --alt \1 %}')
+        document.content = newContent
     end
 end
